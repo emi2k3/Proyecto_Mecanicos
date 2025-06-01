6fb775eb4c9a61643d834d3952813104fa0ccdc1
unknown <emilio.rodriguez@correo.ucu.edu.uy>
2025-06-01 19:01:31 -0300

    React instalado, faltan solucionar errores de ejecución

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+export const API_CONFIG = {
+    baseURL: 'http://10.0.2.2:3000', // IP especial para acceder al localhost desde el emulador de Android
+    timeout: 10000,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { API_CONFIG } from '../config/api';
+
+const api = axios.create(API_CONFIG);
+
+export const login = async (credentials) => {
+  try {
+    const response = await api.post('/auth/login', credentials);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const register = async (userData) => {
+  try {
+    const response = await api.post('/auth/register', userData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
